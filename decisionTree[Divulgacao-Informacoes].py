66ab2cfd7dcba81e1be6fa64f218faea4bf887e7
João Calixto <calixtounicap@gmail.com>
2016-04-07 07:35:40 -0300

    continue
    continue

--- decisionTree[Divulgacao-Informacoes].py
+++ decisionTree[Divulgacao-Informacoes].py
@@ -9,9 +9,9 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 def get_iris_data():
     """Get the iris data, from local csv or pandas repo."""
-    if os.path.exists("iris.csv"):
+    if os.path.exists("treino_3.csv"):
         print("-- iris.csv found locally")
-        df = pd.read_csv("iris.csv", index_col=False)
+        df = pd.read_csv("treino_3.csv", index_col=False)
     else:
         print("-- trying to download from github")
         fn = "https://github.com/joaocalixto/decisionTreeIdentityManagement/blob/master/decisionTree/decisionTree/iris.csv"
@@ -20,7 +20,7 @@ def get_iris_data():
         except:
             exit("-- Unable to download iris.csv")
 
-        with open("iris.csv", 'w') as f:
+        with open("treino_3.csv", 'w') as f:
             print("-- writing to local iris.csv file")
             df.to_csv(f)
 
@@ -60,16 +60,16 @@ print("* df2.head()", df2[["Target", "Name"]].head(), sep="\n", end="\n\n")
 print("* df2.tail()", df2[["Target", "Name"]].tail(), sep="\n", end="\n\n")
 print("* targets", targets, sep="\n", end="\n\n")
 
-features = list(df2.columns[:6])
+features = list(df2.columns[:4])
 print("* features:", features, sep="\n")
 
 y = df2["Target"]
 X = df2[features]
-dt = DecisionTreeClassifier(min_samples_split=7, random_state=99)
-dt.fit(X, y)
+treino3 = DecisionTreeClassifier(min_samples_split=4, random_state=99)
+treino3.fit(X, y)
 
 # 1,0,1,0,1,1
-predict = dt.predict([[1,0,1,0,1,1]])
+predict = treino3.predict([[1,0,1,0]])
 print ("predict ", predict)
 
 def visualize_tree(tree, feature_names, targets):
@@ -80,18 +80,18 @@ def visualize_tree(tree, feature_names, targets):
     tree -- scikit-learn DecsisionTree.
     feature_names -- list of feature names.
     """
-    with open("dt.dot", 'w') as f:
+    with open("treino3.dot", 'w') as f:
         export_graphviz(tree, out_file=f,
                         feature_names=feature_names, class_names=targets)
 
-    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    command = ["dot", "-Tpng", "treino3.dot", "-o", "treino3.png"]
     try:
         subprocess.check_call(command)
     except:
         exit("Could not run dot, ie graphviz, to "
              "produce visualization")
         
-visualize_tree(dt, features, targets)
+visualize_tree(treino3, features, targets)
 
 # generate code
 
@@ -140,4 +140,4 @@ def get_code(tree, feature_names, target_names,
 
     recurse(left, right, threshold, features, 0, 0)
 
-get_code(dt, features, targets)
+get_code(treino3, features, targets)
--- decisionTree_2.py
+++ decisionTree_2.py
@@ -9,9 +9,9 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 def get_iris_data():
     """Get the iris data, from local csv or pandas repo."""
-    if os.path.exists("iris.csv"):
+    if os.path.exists("treino_2.csv"):
         print("-- iris.csv found locally")
-        df = pd.read_csv("iris.csv", index_col=False)
+        df = pd.read_csv("treino_2.csv", index_col=False)
     else:
         print("-- trying to download from github")
         fn = "https://github.com/joaocalixto/decisionTreeIdentityManagement/blob/master/decisionTree/decisionTree/iris.csv"
@@ -60,16 +60,16 @@ print("* df2.head()", df2[["Target", "Name"]].head(), sep="\n", end="\n\n")
 print("* df2.tail()", df2[["Target", "Name"]].tail(), sep="\n", end="\n\n")
 print("* targets", targets, sep="\n", end="\n\n")
 
-features = list(df2.columns[:6])
+features = list(df2.columns[:4])
 print("* features:", features, sep="\n")
 
 y = df2["Target"]
 X = df2[features]
-dt = DecisionTreeClassifier(min_samples_split=7, random_state=99)
-dt.fit(X, y)
+treino_2 = DecisionTreeClassifier(min_samples_split=3, random_state=99)
+treino_2.fit(X, y)
 
 # 1,0,1,0,1,1
-predict = dt.predict([[1,0,1,0,1,1]])
+predict = treino_2.predict([[1,0,1,0]])
 print ("predict ", predict)
 
 def visualize_tree(tree, feature_names, targets):
@@ -80,18 +80,18 @@ def visualize_tree(tree, feature_names, targets):
     tree -- scikit-learn DecsisionTree.
     feature_names -- list of feature names.
     """
-    with open("dt.dot", 'w') as f:
+    with open("treino_2.dot", 'w') as f:
         export_graphviz(tree, out_file=f,
                         feature_names=feature_names, class_names=targets)
 
-    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
+    command = ["dot", "-Tpng", "treino_2.dot", "-o", "treino_2.png"]
     try:
         subprocess.check_call(command)
     except:
         exit("Could not run dot, ie graphviz, to "
              "produce visualization")
         
-visualize_tree(dt, features, targets)
+visualize_tree(treino_2, features, targets)
 
 # generate code
 
@@ -140,4 +140,4 @@ def get_code(tree, feature_names, target_names,
 
     recurse(left, right, threshold, features, 0, 0)
 
-get_code(dt, features, targets)
+get_code(treino_2, features, targets)
